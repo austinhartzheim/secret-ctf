926f15cd009d4da2b9060e565f028b66e96c48d1
Austin Hartzheim <inbox@austinhartzheim.me>
2017-07-08 23:32:04 +0000

    Added password extraction challenge.

---
+++ challenges/extract-from-password-field/src/main.rs
@@ -0,0 +1,18 @@
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
+extern crate rocket;
+use rocket::http::ContentType;
+use rocket::response::content::Content;
+
+#[get("/generate")]
+fn generate() -> Content<String> {
+    let flag = "flag_j29FJ9zi9rjFI832uFfjE";
+    Content(ContentType::HTML,
+            format!("<!DOCTYPE html><html><body><form><input type=\"password\" value=\"{}\" /></form></body></html>",
+                    flag))
+}
+
+fn main() {
+    rocket::ignite().mount("/", routes![generate]).launch();
+}
